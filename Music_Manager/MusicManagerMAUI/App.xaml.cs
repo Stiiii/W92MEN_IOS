cf03216270b6cd93974f8b2f0e758ac593f1d4cd
Szalai István <48644228+Stiiii@users.noreply.github.com>
2024-05-29 18:21:42 +0200

    .NET MAUI App added and gitignore

---
+++ Music_Manager/MusicManagerMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicManagerMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
